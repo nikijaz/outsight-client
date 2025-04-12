04d1cb1299a7874037b1d24cbf49338c0cc57639
Mikita Sirosh <mikita.sirosh@gmail.com>
2025-04-12 14:03:15 +0200

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  stylistic.configs.customize({
+    semi: true,
+  }),
+  {
+    rules: {
+      'react/self-closing-comp': ['error'],
+    },
+  },
+);
